9403cadfb369bce2177a497f1df200b295eb3f6b
Ranjan Yadav <yadav.ranjan25@gmail.com>
2022-10-01 12:30:36 +0300

    Created a variable.js file. Learned about assigning variables and completed the variable assignment

---
+++ JS_Fundamentals_1/variable.js
@@ -0,0 +1,18 @@
+// variables
+
+let firstName = "jonas";
+
+const PI = 22 / 7;
+
+console.log("jonas");
+console.log(PI);
+
+//Assignment
+
+let country = "Nepal";
+let continent = "Asia";
+let population = 29.14;
+
+console.log(country);
+console.log(continent);
+console.log(population);
