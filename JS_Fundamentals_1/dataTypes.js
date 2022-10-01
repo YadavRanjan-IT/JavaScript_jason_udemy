5dcaeb098464d08388f1f33b59ff8f1779c62c11
Ranjan Yadav <yadav.ranjan25@gmail.com>
2022-10-01 16:35:16 +0300

    Created a file to learn about the data types and completed its assignments

---
+++ JS_Fundamentals_1/dataTypes.js
@@ -0,0 +1,45 @@
+// data Types
+
+// Numbers either floating( decimal ) or integers
+// String group of characters in quotes "ranjan" or 'ranjan'
+// boolean true or false
+// undefined variable without value assigned
+// NULL variable with empty value
+
+let hasGun = false;
+
+console.log(typeof hasGun);
+console.log(typeof true);
+console.log(typeof false);
+
+let num = 23;
+
+console.log(typeof num);
+console.log(typeof 23.184658);
+
+let firstName = "JavaScript";
+
+console.log(typeof firstName);
+
+let lastName;
+
+console.log(typeof lastName);
+
+lastName = null;
+
+console.log(typeof lastName); // bug in JS
+
+//Assignments
+
+let country = "Nepal";
+let continent = "Asia";
+let population = 29.14;
+
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof continent);
+console.log(typeof isIsland);
+console.log(typeof language);
