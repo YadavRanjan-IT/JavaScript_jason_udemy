2c2b03ab75cbbb2d19bc608611929229c8f2a1bf
Ranjan Yadav <yadav.ranjan25@gmail.com>
2022-10-07 11:39:17 +0300

    Learnt about truthy and falsy values

--- JS_Fundamentals_1/types.js
+++ JS_Fundamentals_1/types.js
@@ -27,3 +27,41 @@ console.log(result);
 
 result = 5 + 6 + "4" + 9 - 4 - 2; // 1143 number
 console.log(result);
+
+// truthy and falsy values
+
+// falsy values empty String '', null, NaN, 0, undefined.
+
+if (NaN) {
+  console.log(`NaN is a truthy value`);
+} else {
+  console.log(`NaN is a falsy value`);
+}
+
+let x; // x ia undefined
+if (x) {
+  console.log(`undefined is a truthy value`);
+} else {
+  console.log(`undefined is a falsy value`);
+}
+
+if (0) {
+  console.log(`0 is a truthy value`);
+} else {
+  console.log(`0 is a falsy value`);
+}
+
+let y = null;
+if (y) {
+  console.log(`null is a truthy value`);
+} else {
+  console.log(`null is a falsy value`);
+}
+
+if ("") {
+  console.log(`'' is a truthy value`);
+} else {
+  console.log(`'' is a falsy value`);
+}
+
+// rest are truthy values
