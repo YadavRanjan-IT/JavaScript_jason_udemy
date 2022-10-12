85ca9619dea0f145beb5b4e3979ecacfdb9d733a
Ranjan Yadav <yadav.ranjan25@gmail.com>
2022-10-12 13:29:18 +0300

    Learnt about ternary opeartor and completed its assignments

---
+++ JS_Fundamentals_1/conditional.js
@@ -0,0 +1,25 @@
+// TERNARY OPERATOR
+
+28 >= 18 ? true : false;
+
+let age = 24;
+
+const drink = age >= 18 ? "wine🍷" : "water💦";
+
+// ASSIGNMENTS
+
+let population = 29.14;
+
+population > 33
+  ? console.log("Nepal's population is above average")
+  : console.log("Nepal's population is below average");
+
+population = 13;
+population > 33
+  ? console.log("Nepal's population is above average")
+  : console.log("Nepal's population is below average");
+
+population = 130;
+population > 33
+  ? console.log("Nepal's population is above average")
+  : console.log("Nepal's population is below average");
